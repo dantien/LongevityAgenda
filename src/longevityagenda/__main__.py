a1c2265d4f36add0fe35f556ab88c8636b46f108
Terje Pettersen <tepe75@gmail.com>
2023-03-01 08:45:34 +0000

    Updated __main__.py

--- src/longevityagenda/__main__.py
+++ src/longevityagenda/__main__.py
@@ -1,4 +1,17 @@
-from longevityagenda.app import main
+from longevityagenda.views.about_view import AboutView
+from longevityagenda.views.calendar_view import CalendarView
+from longevityagenda.views.challenges_view import ChallengesView
+from longevityagenda.views.fitness_view import FitnessView
+from longevityagenda.views.login_view import LoginView
+from longevityagenda.views.main_view import MainView
+from longevityagenda.views.profile.profile_view import ProfileView
+from longevityagenda.views.progress_view import ProgressView
+from longevityagenda.views.settings_view import SettingsView
+from longevityagenda.views.shop_view import ShopView
+from longevityagenda.views.signup_view import SignupView
 
 if __name__ == '__main__':
-    main().main_loop()
+    views = [AboutView, CalendarView, ChallengesView, FitnessView, LoginView,
+             MainView, ProfileView, ProgressView, SettingsView, ShopView, SignupView]
+    for view in views:
+        view().present()
